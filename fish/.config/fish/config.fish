bfb90b061816e9f940494ac4256ef4252088c752
osmarmora05 <osmaradrianmoracerna@gmail.com>
2025-01-14 16:34:35 -0600

    feat(fish): add zoxide & enable starship

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    # starship init fish | source	
-    oh-my-posh init fish --config "~/.config/oh-my-posh.toml" | source
+    zoxide init fish | source
+    starship init fish | source	
+    # oh-my-posh init fish --config "~/.config/oh-my-posh.toml" | source
     # Set the Android SDK home directory
     # set -x ANDROID_HOME $HOME/Android/Sdk
     # Add Android SDK tools, emulator, and platform-tools to PATH
