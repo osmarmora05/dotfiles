4efc861f456047ae92de3395bbd04e2f1650a10c
osmarmora05 <osmaradrianmoracerna@gmail.com>
2025-02-09 20:59:58 -0600

    feat(nvim): add format setup (conform.nvim)

---
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -0,0 +1,32 @@
+return {
+  "stevearc/conform.nvim",
+  opts = function()
+    local opts = {
+      default_format_opts = {
+        timeout_ms = 3000,
+        async = false,
+        quiet = false,
+        lsp_format = "fallback",
+      },
+      formatters_by_ft = {
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        svelte = { "prettier" },
+        css = { "prettier" },
+        html = { "prettier" },
+        json = { "prettier" },
+        yaml = { "prettier" },
+        markdown = { "prettier" },
+        graphql = { "prettier" },
+        liquid = { "prettier" },
+        lua = { "stylua" },
+      },
+      formatters = {
+        injected = { options = { ignore_errors = true } },
+      },
+    }
+    return opts
+  end,
+}
