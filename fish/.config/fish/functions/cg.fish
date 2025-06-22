f4f877bf7d6ca767c28f6b0edbd82b78df877a0d
osmarmora05 <osmaradrianmoracerna@gmail.com>
2025-06-22 15:08:19 -0600

    feat(fish): alias cg=cargo wraper: cg r, cg t, cg b, cg c

--- fish/.config/fish/functions/cg.fish
+++ fish/.config/fish/functions/cg.fish
@@ -1,3 +1,12 @@
-function cg --description "alias cg=cargo"
-  cargo $argv
+function cg --description "alias cg=cargo wrapper: cg r, cg t, cg b, cg c"
+    switch $argv[1]
+        case r
+            cargo run $argv[2..]
+        case t
+            cargo test $argv[2..]
+        case b
+            cargo build $argv[2..]
+        case c
+            cargo check $argv[2..]
+    end
 end
